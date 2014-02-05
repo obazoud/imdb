5e45762d909bbe9dcb788db205078f849e923dae
Olivier Bazoud <olivier.bazoud@gmail.com>
2014-02-05 18:22:57 +0100

    Fix target in download script

--- download.sh
+++ download.sh
@@ -5,10 +5,11 @@ BASE=`dirname $0`
 MIRROR="ftp.fu-berlin.de/misc/movies/database"
 
 FILES=( "actors" "actresses" "aka-names" "aka-titles" "alternate-versions" "biographies" "business" "certificates" "cinematographers" "color-info" "complete-cast" "complete-crew" "composers" "costume-designers" "countries" "crazy-credits" "directors" "distributors" "editors" "genres" "german-aka-titles" "goofs" "iso-aka-titles" "italian-aka-titles" "keywords" "language" "laserdisc" "literature" "locations" "miscellaneous-companies" "miscellaneous" "movie-links" "movies" "mpaa-ratings-reasons" "plot" "producers" "production-companies" "production-designers" "quotes" "ratings" "release-dates" "running-times" "sound-mix" "soundtracks" "special-effects-companies" "taglines" "technical" "trivia" "writers" )
-mkdir -p ${BASE}/rawimdb
+TARGET="${BASE}/rawimdb"
+mkdir -p ${TARGET}
 
 for file in ${FILES[@]}
 do
-	wget --directory-prefix "${BASE}/raw" --timestamping --continue "ftp://${MIRROR}/${file}.list.gz"
+	wget --directory-prefix "${TARGET}" --timestamping --continue "ftp://${MIRROR}/${file}.list.gz"
 done;
 
