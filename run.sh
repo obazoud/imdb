371cb78a36d378d5f88dbf2948cb2bfe8958b9f6
Olivier Bazoud <olivier.bazoud@gmail.com>
2014-02-07 17:11:10 +0100

    Add time

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 set -x
 
 /usr/local/bin/imdbpy2sql.py -o sqlobject -u mysql://root:root@localhost/imdb -d /vagrant/rawimdb --mysql-force-myisam
+# 61s user 10755.56s system 46% cpu 13:21:20.06 total
